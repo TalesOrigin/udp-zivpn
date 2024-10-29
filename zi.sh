8f3e5d56d13677ab216fe9848d32a4f427697d17
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2024-10-29 15:28:58 +0630

    Update zi.sh

--- zi.sh
+++ zi.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Zivpn UDP Module installer
 # Creator Zahid Islam
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+plain='\033[0m'
+NC='\033[0m' # No Color
 
 echo -e "Updating server"
 sudo apt-get update && apt-get upgrade -y
